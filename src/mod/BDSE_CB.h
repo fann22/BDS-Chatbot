0877fc1f76490a26a4cbf00c05cdb2cbeb234144
Fann22 <rilisan5@gmail.com>
2026-05-30 14:08:41 +0700

    Initial commit.

---
+++ src/mod/BDSE_CB.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ll/api/mod/NativeMod.h"
+
+namespace bds_chatbot {
+
+class BDS_CB {
+
+public:
+    static BDS_CB& getInstance();
+
+    BDS_CB() : mSelf(*ll::mod::NativeMod::current()) {}
+
+    [[nodiscard]] ll::mod::NativeMod& getSelf() const { return mSelf; }
+
+    /// @return True if the mod is loaded successfully.
+    bool load();
+
+    /// @return True if the mod is enabled successfully.
+    bool enable();
+
+    /// @return True if the mod is disabled successfully.
+    bool disable();
+
+    // TODO: Implement this method if you need to unload the mod.
+    // /// @return True if the mod is unloaded successfully.
+    // bool unload();
+
+private:
+    ll::mod::NativeMod& mSelf;
+};
+
+} // namespace bds_chatbot
---
+++ src/mod/BDS_CB.cpp
@@ -0,0 +1,54 @@
+#include "mod/BDS_CB.h"
+
+#include <ll/api/service/Bedrock.h>
+
+#include <ll/api/event/player/PlayerChatEvent.h>
+
+namespace bds_chatbot {
+
+BDS_CB& BDS_CB::getInstance() {
+    static BDSE instance;
+    return instance;
+}
+
+static std::vector<ll::event::ListenerPtr> gListeners;
+
+bool BDS_CB::load() {
+    // getSelf().getLogger().debug("Loading...");
+    return true;
+}
+
+bool BDS_CB::enable() {
+    // getSelf().getLogger().info("loaded.");
+    gListeners.insert(
+        gListeners.begin(),
+        bus.emplaceListener<ll::event::PlayerChatEvent>([](ll::event::PlayerChatEvent& event) {
+            // BDSE::getInstance().getSelf().getLogger().info("{}: {}", event.self().getRealName(), event.message());
+            // auto message = "§b" + event.self().getRealName() + "§f: " + event.message();
+            // TextPacket::createRawMessage(message).sendToClients();
+            // event.cancel();
+            if (event.message().rfind("ai ", 0) == 0) {
+                std::string query = msg.substr(3);
+                BDS_CB::getInstance().getSelf().getLogger().info("Prompt from {}: {}", event.self().getRealName(), query);
+            }
+        })
+    );
+    return true;
+}
+
+bool BDS_CB::disable() {
+    auto& bus = ll::event::EventBus::getInstance();
+
+    for (auto& listener : gListeners) {
+        bus.removeListener(listener);
+        listener.reset();
+    }
+
+    gListeners.clear();
+
+    return true;
+}
+
+} // namespace bds_chatbot
+
+LL_REGISTER_MOD(bds_chatbot::BDS_CB, bds_chatbot::BDS_CB::getInstance());
